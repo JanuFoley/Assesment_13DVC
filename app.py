b86ad2aaf1faa2dba8038d571a67661c068ef61a
JanuFoley <127055740+JanuFoley@users.noreply.github.com>
2024-09-04 13:04:29 +1200

    links

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def Home_Pagwe():  # put application's code here
-    return render_template('base.html')
+def Home_Page():
+    return render_template('index.html')
+
+@app.route('/Accessability')
+def Accessability_page():
+    return render_template('Accessability.html')
+
+@app.route('/Location')
+def Location_page():
+    return render_template('Location.html')
 
 if __name__ == '__main__':
     app.run()
